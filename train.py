5d12c2b059eb748b8b134e4ef56a60760d936e83
Sebastian Dziura <sabek12@gmail.com>
2025-03-13 11:50:08 +0100

    Add cross-validation

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
 import h5py
 import torch
-from torch.utils.data import DataLoader, TensorDataset, random_split
+from torch.utils.data import DataLoader, TensorDataset, Subset
 import pytorch_lightning as pl
+from sklearn.model_selection import StratifiedKFold
 
 from model import TwoLayerModel
 
@@ -19,18 +20,21 @@ y = torch.tensor(y, dtype=torch.long)
 print("Observations shape:", observations.shape)
 print("Labels shape:", y.shape)
 
-
-# Create a PyTorch Dataset & DataLoader
+# Convert dataset into a list of indices for cross-validation
 dataset = TensorDataset(observations, y)
-train, val, test = random_split(
-    dataset, [0.7, 0.2, 0.1], generator=torch.Generator().manual_seed(42)
-)
-train_loader = DataLoader(train, batch_size=32, shuffle=True)
-val_loader = DataLoader(test, batch_size=32, shuffle=True)
-test_loader = DataLoader(test, batch_size=32, shuffle=True)
+skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
 
-model = TwoLayerModel(input_size=20)
+for fold, (train_idx, val_idx) in enumerate(skf.split(observations, y)):
+    print(f"\nFold {fold+1}")
+    
+    train_subset = Subset(dataset, train_idx)
+    val_subset = Subset(dataset, val_idx)
 
-trainer = pl.Trainer(max_epochs=10, accelerator="auto", val_check_interval=1)
-trainer.fit(model, train_loader, val_loader)
-trainer.test(model, test_loader)
+    # Create a PyTorch Dataset & DataLoader
+    train_loader = DataLoader(train_subset, batch_size=32, shuffle=True)
+    val_loader = DataLoader(val_subset, batch_size=32, shuffle=True)
+
+    model = TwoLayerModel(input_size=20)
+
+    trainer = pl.Trainer(max_epochs=10, accelerator="auto", val_check_interval=1)
+    trainer.fit(model, train_loader, val_loader)
