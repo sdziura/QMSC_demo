b9ef78ce1ea0ba85bdd53f34a49b7f3ac3f0ddf4
Sebastian Dziura <sabek12@gmail.com>
2025-03-18 11:37:58 +0100

    Modify code according to clean code recomendations

--- train.py
+++ train.py
@@ -8,7 +8,23 @@ from sklearn.model_selection import StratifiedKFold
 import mlflow
 from mlflow.models import infer_signature
 
+import logging
+from dataclasses import dataclass
+
 from model import TwoLayerModel
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+
+@dataclass
+class TrainParams:
+    max_iter: int = 1
+    folds: int = 2
+    random_state: int = 42
+    batch_size: int = 32
+    val_check_interval: int = 1
+    log_every_n_steps: int = 1
 
 
 class Train:
@@ -17,97 +33,97 @@ class Train:
         mlflow.set_experiment("HMM_Classification")
         mlflow.pytorch.autolog()
 
-        self.params = {
-            "max_iter": 1,
-            "folds": 1,
-            "random_state": 42,
-            "batch_size": 32,
-            "val_check_interval": 1,
-            "log_every_n_steps": 10,
-        }
+        self.params = TrainParams()
         self.load_data()
 
-    def load_data(self, file_path="hmm_gaussian_chains.h5"):
+    def load_data(self, file_path: str = "hmm_gaussian_chains.h5") -> None:
         with h5py.File(file_path, "r") as f:
-            labels = f["label"][:]
-            observations = f["observed"][:]
+            labels, observations = f["label"][:], f["observed"][:]
 
         # Flatten each (10,2) sequence into a 1D vector of size 20
         observations = observations.reshape(observations.shape[0], -1)
         self.X = torch.tensor(observations, dtype=torch.float32)
         self.y = torch.tensor(labels, dtype=torch.long)
 
-        print("Observations shape:", self.X.shape)
-        print("Labels shape:", self.y.shape)
+        logger.info(f"Observations shape: {self.X.shape}")
+        logger.info(f"Labels shape: {self.y.shape}")
 
-    def train(self):
+    def train(self) -> None:
         # Convert dataset into a list of indices for cross-validation
         dataset = TensorDataset(self.X, self.y)
         skf = StratifiedKFold(
-            n_splits=self.params["folds"],
+            n_splits=self.params.folds,
             shuffle=True,
-            random_state=self.params["random_state"],
+            random_state=self.params.random_state,
         )
 
         for fold, (train_idx, val_idx) in enumerate(skf.split(self.X, self.y)):
-            print(f"\nFold {fold+1}")
-
-            train_subset = Subset(dataset, train_idx)
-            val_subset = Subset(dataset, val_idx)
+            logger.info(f"\nFold {fold+1}")
 
             # Create a PyTorch Dataset & DataLoader
-            train_loader = DataLoader(
-                train_subset,
-                batch_size=self.params["batch_size"],
-                shuffle=True,
-                num_workers=4,
+            train_loader = self.get_dataloader(
+                dataset=dataset, indices=train_idx, shuffle=True
             )
-            val_loader = DataLoader(
-                val_subset,
-                batch_size=self.params["batch_size"],
-                shuffle=False,
-                num_workers=4,
+            val_loader = self.get_dataloader(
+                dataset=dataset, indices=val_idx, shuffle=False
             )
 
-            model = TwoLayerModel(input_size=self.X.shape[1])
+            self.train_fold(fold, train_loader, val_loader)
 
-            # Define a TensorBoard logger
-            tb_logger = TensorBoardLogger("logs/", name="my_model")
+    def train_fold(
+        self, fold: int, train_loader: DataLoader, val_loader: DataLoader
+    ) -> None:
+        model = TwoLayerModel(input_size=self.X.shape[1])
 
-            # Start MLFlow run
-            with mlflow.start_run(run_name=f"Fold_{fold+1}"):
-                mlflow.log_params(self.params)  # Log hyperparameters
-                trainer = pl.Trainer(
-                    max_epochs=self.params["max_iter"],
-                    accelerator="auto",
-                    val_check_interval=self.params["val_check_interval"],
-                    log_every_n_steps=self.params["log_every_n_steps"],
-                    logger=tb_logger,
-                    callbacks=[
-                        pl.callbacks.ModelCheckpoint(
-                            dirpath="checkpoints",
-                            filename="best_model",
-                            save_top_k=1,
-                            monitor="val_loss",
-                            mode="min",
-                        )
-                    ],
-                )
+        # Define a TensorBoard logger
+        tb_logger = TensorBoardLogger("logs/", name="my_model")
 
-                trainer.fit(model, train_loader, val_loader)
+        # Start MLFlow run
+        with mlflow.start_run(run_name=f"Fold_{fold+1}"):
+            mlflow.log_params(self.params.__dict__)  # Log hyperparameters
+            trainer = pl.Trainer(
+                max_epochs=self.params.max_iter,
+                accelerator="auto",
+                val_check_interval=self.params.val_check_interval,
+                log_every_n_steps=self.params.log_every_n_steps,
+                logger=tb_logger,
+                callbacks=[
+                    pl.callbacks.ModelCheckpoint(
+                        dirpath="checkpoints",
+                        filename="best_model",
+                        save_top_k=1,
+                        monitor="val_loss",
+                        mode="min",
+                    )
+                ],
+            )
 
-                # Log model with input example
-                input_example = torch.randn(1, self.X.shape[1])
-                input_exampe_np = input_example.numpy()
-                signature = infer_signature(
-                    input_exampe_np, model(input_example).detach().numpy()
-                )
-                mlflow.pytorch.log_model(
-                    model,
-                    f"model_fold_{fold+1}",
-                    input_example=input_exampe_np,
-                    signature=signature,
-                )
+            trainer.fit(model, train_loader, val_loader)
+
+            # Log model with input example
+            self.log_mlflow_model(model=model, fold=fold)
+
+    def log_mlflow_model(self, model: pl.LightningModule, fold: int) -> None:
+        input_example = torch.randn(1, self.X.shape[1])
+        input_example_np = input_example.numpy()
+        signature = infer_signature(
+            input_example_np, model(input_example).detach().numpy()
+        )
+
+        mlflow.pytorch.log_model(
+            model,
+            f"model_fold_{fold+1}",
+            input_example=input_example_np,
+            signature=signature,
+        )
+
+    def get_dataloader(
+        self, dataset: TensorDataset, indices: np.ndarray, shuffle: bool
+    ) -> DataLoader:
+        subset = Subset(dataset, indices)
+        return DataLoader(
+            subset, batch_size=self.params.batch_size, shuffle=shuffle, num_workers=4
+        )
 
 
 if __name__ == "__main__":
