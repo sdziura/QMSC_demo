ef18c1aacae9561f5d595e341f17a62cbcddf103
sdziura <sabek12@gmail.com>
2025-05-10 11:08:48 +0200

    Clean comments

--- utils/optuna_utils.py
+++ utils/optuna_utils.py
@@ -8,12 +8,11 @@ from training.train import Trainer
 def optimize_hyperparameters_NN(trainer: Trainer, n_trials: int) -> dict:
     def objective(trial, trainer: Trainer) -> float:
         optuna_params = NNParams(
-            # learning_rate=trial.suggest_loguniform("learning_rate", 1e-5, 1e-3),
+            learning_rate=trial.suggest_loguniform("learning_rate", 1e-5, 1e-3),
             hidden_size_1=trial.suggest_categorical("hidden_size_1", [16, 32, 64, 128]),
             hidden_size_2=trial.suggest_categorical("hidden_size_2", [16, 32, 64, 128]),
-            # hidden_size_3=trial.suggest_categorical("hidden_size_3", [16, 32, 64, 128]),
             batch_size=trial.suggest_categorical("batch_size", [16, 32, 64, 128]),
-            # dropout=trial.suggest_uniform("dropout", 0.1, 0.5),
+            dropout=trial.suggest_uniform("dropout", 0.1, 0.5),
         )
         val_loss, _ = trainer.train("nn", optuna_params, trial_number=trial.number)
         return val_loss
