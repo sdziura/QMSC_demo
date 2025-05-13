333050521dbc223a0006ceb84f8f3edeee664c98
sdziura <sabek12@gmail.com>
2025-05-13 09:14:01 +0200

    Add load_params function

--- utils/parameters_loader.py
+++ utils/parameters_loader.py
@@ -6,3 +6,23 @@ def save_params(params: dict, file_name: str):
     with open(f"saved_model_params/{file_name}", "w") as fp:
         json.dump(params, fp)
         print(f"Model params saved to the {file_name}")
+
+
+def load_params(file_name: str) -> dict:
+    """
+    Load the dictionary of parameters from a file.
+
+    Parameters
+    ----------
+    file_name : str
+        The name of the file to load the parameters from.
+
+    Returns
+    -------
+    dict
+        The dictionary of loaded parameters.
+    """
+    with open(f"saved_model_params/{file_name}", "r") as fp:
+        params = json.load(fp)
+        print(f"Model params loaded from the {file_name}")
+    return params
