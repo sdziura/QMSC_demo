c5ae57d4ba7248dd42dc751a397a3ec1fba39a14
sdziura <sabek12@gmail.com>
2025-05-17 17:24:19 +0200

    Add type checking function

--- utils/cuda_utils.py
+++ utils/cuda_utils.py
@@ -5,3 +5,11 @@ def set_cuda():
     torch.multiprocessing.set_start_method("spawn", force=True)
     torch.set_float32_matmul_precision("medium")
     print(torch.cuda.get_device_name(torch.cuda.current_device()))
+
+
+def check_type(a, str: str = ""):
+    print(str)
+    if isinstance(a, torch.Tensor):
+        print(f"A is torch.Tensor, device: {a.device}, dtype: {a.dtype}")
+    else:
+        print(f"A type: {type(a)}")
